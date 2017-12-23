655e27d87c681925b4aa10aab37d8557c62ee8c2
Nika Ptskialadze <nptskial@itcollege.ee>
2017-12-23 19:57:27 +0200

    Update UI
    Added styles from material-kit package and updated UI

--- CourseProject/wwwroot/js/site.js
+++ CourseProject/wwwroot/js/site.js
@@ -1,1 +1,32 @@
-﻿// Write your JavaScript code.
+﻿// Datepicker functions
+$('#datePicker').datepicker({
+    todayBtn: "linked",
+    clearBtn: true,
+    autoclose: true,
+    todayHighlight: true,
+    startDate: new Date()
+}).on('changeDate', showStartDate);
+
+$('#datePicker2').datepicker({
+    todayBtn: "linked",
+    clearBtn: true,
+    autoclose: true,
+    todayHighlight: true,
+    startDate: new Date()
+}).on('changeDate', showEndDate);
+
+function showStartDate() {
+    var value = $('#datePicker').datepicker('getFormattedDate');
+    $('#StartDate').val(value);
+    if (value) {
+        $('#datePicker2').datepicker('setStartDate', value);
+    } else {
+        var date = new Date();
+        $('#datePicker2').datepicker('setStartDate', date);
+    }
+}
+
+function showEndDate() {
+    var value = $('#datePicker2').datepicker('getFormattedDate');
+    $('#EndDate').val(value);
+}
